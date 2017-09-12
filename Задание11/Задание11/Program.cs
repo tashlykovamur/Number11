ac8f8f53b9b879010da58d400e9e004cf33fb519
Аня Ташлыкова <tashlykova-anna@mail.ru>
2017-09-12 20:34:13 +0500

    Дописана проверка на ввод в обоих функциях

--- Задание11/Задание11/Program.cs
+++ Задание11/Задание11/Program.cs
@@ -33,76 +33,104 @@ namespace Задание11
 
         static void Shifrovanie()
         {
-            Console.WriteLine("Введите число");
+
+            int check = 0;
+
+            while (check <1)
+            {     Console.WriteLine("Введите число");
             string num = Console.ReadLine();
-
-            int check = Check(num);
-            if (check == 1)
-            {
-                int[] arr = new int[num.Length];
-
-                for (int i = 0; i < num.Length; i++)
+             check = Check(num);
+            
+                if (check == 1)
                 {
-                    arr[i] = int.Parse(num[i].ToString());
+                    int[] arr = new int[num.Length];
+
+                    for (int i = 0; i < num.Length; i++)
+                    {
+                        arr[i] = int.Parse(num[i].ToString());
+                    }
+
+                    int[] newArr = new int[num.Length];
+                    newArr[0] = arr[0];
+
+                    for (int i = 1; i < num.Length; i++)
+                    {
+                        if (arr[i] == arr[i - 1])
+                            newArr[i] = 1;
+                        else
+                            newArr[i] = 0;
+                    }
+
+                    Console.WriteLine("Зашифрованная последовательность:");
+                    for (int i = 0; i < num.Length; i++)
+                    {
+                        Console.Write(newArr[i]);
+                    }
+                    Console.WriteLine();
+
+                    
+                }
+                else if (check == -1)
+                {
+                    Console.WriteLine("Ошибка. Вы ввели не число.");
+                }
+                else if (check == -2)
+                {
+                    Console.WriteLine("Ошибка. Число должно состоять только из 0 и 1");
                 }
 
-                int[] newArr = new int[num.Length];
-                newArr[0] = arr[0];
-
-                for (int i = 1; i < num.Length; i++)
-                {
-                    if (arr[i] == arr[i - 1])
-                        newArr[i] = 1;
-                    else
-                        newArr[i] = 0;
-                }
-
-                string newNum = string.Join("", newArr);
-
-                Console.WriteLine($"Зашифрованная последовательность = {newNum}");
             }
-            else if (check == -1)
-            {
-
-            }
-
         }
-
         static void Deshifrovanie()
         {
-            Console.WriteLine("Введите число");
-            string num = Console.ReadLine();
+            int check = 0;
 
-            int[] arr = new int[num.Length];
-
-            for (int i = 0; i < num.Length; i++)
+            while (check < 1)
             {
-                arr[i] = int.Parse(num[i].ToString());
+                Console.WriteLine("Введите число");
+                string num = Console.ReadLine();
+                check = Check(num);
+
+                if (check == 1)
+                {
+                    int[] arr = new int[num.Length];
+
+                    for (int i = 0; i < num.Length; i++)
+                    {
+                        arr[i] = int.Parse(num[i].ToString());
+                    }
+
+                    int[] newArr = new int[num.Length];
+                    newArr[0] = arr[0];
+
+                    for (int i = 1; i < num.Length; i++)
+                    {
+                        if (arr[i] == 0)//если ноль, то отличаются с предыдущим
+                            newArr[i] = newArr[i - 1] == 1 ? 0 : 1; //если пред = 0 то 1, а если 1 то 0
+                         //Convert.ToInt32((!Convert.ToBoolean(newArr[i - 1])));
+                        else
+                            newArr[i] = newArr[i - 1];//если эл-т 1, то повторяем пред
+                    }
+                    //string newNum = string.Join("", newArr);
+
+                    Console.WriteLine($"Расшифрованная последовательность:");
+                    for (int i = 0; i < num.Length; i++)
+                    {
+                        Console.Write(newArr[i]);
+                    }
+                    Console.WriteLine();
+                }
+                else if (check == -1)
+                {
+                    Console.WriteLine("Ошибка. Вы ввели не число.");
+                }
+                else if (check == -2)
+                {
+                    Console.WriteLine("Ошибка. Число должно состоять только из 0 и 1");
+                }
+
             }
-
-            int[] newArr = new int[num.Length];
-            newArr[0] = arr[0];
-
-            for (int i = 1; i < num.Length; i++)
-            {
-                if (arr[i] == 0)//если ноль, то отличаются с предыдущим
-                    newArr[i] = newArr[i - 1] == 1 ? 0 : 1; //если пред = 0 то 1, а если 1 то 0
-                //Convert.ToInt32((!Convert.ToBoolean(newArr[i - 1])));
-                else
-                    newArr[i] = newArr[i - 1];//если эл-т 1, то повторяем пред
-            }
-
-            //for (int i = 0; i < num.Length; i++)
-            //{
-            //    Console.Write(newArr[i]);
-            //}
-            //Console.WriteLine();
-
-            string newNum = string.Join("", newArr);
-
-            Console.WriteLine($"Расшифрованная последовательность = {newNum}");
         }
-
         static void Main(string[] args)
         {
             Shifrovanie();
